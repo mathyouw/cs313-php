75999b0db3b77abc5d0d4f8847eb1093d0e996a5
Mathyouw <wil15171@byui.edu>
2019-01-16 23:19:55 -0700

    added team02 assignment

---
+++ web/myscripts.js
@@ -0,0 +1,18 @@
+// this is an empty js file.
+
+function clickMe() {
+	alert("Clicked!");
+}
+
+function changeColor() {
+	var textbox_id = "txtColor";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
+
+}
