0c2aebcfcd6bece4c436a562593fda0f2f3dd5e8
Mathyouw <wil15171@byui.edu>
2019-01-26 13:09:21 -0700

    testing my week 03 assignment php

---
+++ web/assign03php.php
@@ -0,0 +1,34 @@
+
+/**
+ * Created by PhpStorm.
+ * User: mathyouw
+ * Date: 1/26/2019
+ * Time: 12:52 PM
+ */
+
+<html>
+<head>
+
+
+</head>
+<body>
+<?php
+$username = $_POST['name'];
+$email = $_POST['email'];
+$streetAddress = $_POST['streetAddress'];
+$City = $_POST['City'];
+$State = $_POST['State'];
+$zipcode = $_POST{'zipcode'}
+
+
+
+echo "$username<br>";
+echo "$email<br>";
+echo "$streetAddress<br>";
+echo "$City<br>";
+echo "$zipcode";
+
+?>
+
+</body>
+</html>
