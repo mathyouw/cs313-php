cef6bd429e382b2feb80dec20c6a16afd9199af4
Mathyouw <wil15171@byui.edu>
2019-01-26 20:58:16 -0700

    assign03 php not working

--- web/assign03php.php
+++ web/assign03php.php
@@ -14,6 +14,12 @@ $streetAddress = $_POST['streetAddress'];
 $City = $_POST['City'];
 $State = $_POST['State'];
 $zipcode = $_POST{'zipcode'};
+$camaro = $_SESSION['camaro'];
+$F150 = $_SESSION['F150']; 
+$Lesabre = $_SESSION['Lesabre'];
+$RV = $_SESSION['RV'];
+$Fusion = $_SESSION['Fusion'];
+$Honda = $_SESSION['Honda'];
 
 
 
@@ -23,6 +29,12 @@ echo "$streetAddress<br>";
 echo "$City<br>";
 echo "$State<br>";
 echo "$zipcode";
+echo "$camaro<br>";
+   echo "$F150<br>";
+   echo "$Lesabre<br>";
+   echo "$RV<br>";
+   echo "$Fusion<br>";
+   echo "$Honda<br>";
 
 ?>
 
