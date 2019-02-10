00115108e1c8794d13ee3f565f7babfc5b2c7727
Mathyouw <wil15171@byui.edu>
2019-02-09 21:44:07 -0700

    change

--- web/dbconect.php
+++ web/dbconect.php
@@ -15,12 +15,13 @@ function getDBConnection(){
       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
     }
     catch (PDOException $ex)
     {
       echo 'Error!: ' . $ex->getMessage();
       die();
     }
-    return $db;
+
 }
 ?>
