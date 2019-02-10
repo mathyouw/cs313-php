e0d6b69bf167385d9c6a3eb05bbf1df765474b06
Mathyouw <wil15171@byui.edu>
2019-02-09 20:55:02 -0700

    adding db access files

---
+++ web/PHPDataAccess.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+    <?php
+    include '../dbconect.php';
+    try {
+        $db = getDBConnection();
+    }
+    catch(Exception $exception){
+        echo $exception->getMessage();
+    }
+   <!--  $searchBook = $_GET['book'];
+    foreach ($db->query("select * from public.SCRIPTURES where book = '$searchBook'") as $row){
+        $book = $row['book'];
+        $chapter = $row['chapter'];
+        $verse = $row['verse'];
+        $content = $row['content'];
+        $id = $row['id'];
+        echo '<a href="scrip_details.php?id=' .$id. '">' . $book. ' '.$chapter.':'. $verse. '</a><br>';
+    }; -->
+    ?>
+
+    <form method="GET" action="scripture.php">
+        <input type="text" name="book">
+
+        <button type="submit">Submit</button>
+    </form>
+
+    </body>
+
+</html>
