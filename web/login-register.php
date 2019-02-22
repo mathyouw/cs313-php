c89ac5b75b25e77f65e733d3240301be0d777796
Mathyouw <wil15171@byui.edu>
2019-02-21 20:50:53 -0700

    test2

--- web/login-register.php
+++ web/login-register.php
@@ -48,7 +48,7 @@
             </div>
         </fieldset>
     </form>
-<form action="welcome.php" method="post">
+<form action="PHPDataAccess.php" method="post">
     Name: <input type="text" name="name"><br>
     E-mail: <input type="text" name="email"><br>
     <input type="submit">
