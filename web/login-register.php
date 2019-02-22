752525a6d08e2f0984ce3ba9bef1f62583f83995
Mathyouw <wil15171@byui.edu>
2019-02-21 20:17:41 -0700

    testing

--- web/login-register.php
+++ web/login-register.php
@@ -1,41 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <title>Assignment05</title>
-   
-</head>
-    <body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mathyouw
+ * Date: 2/21/2019
+ * Time: 8:12 PM
+ */
 
-    <?php
-
-    echo '<p> hello world </p>';
-    include 'dbconect.php';
-    try {
-//        echo "no error1";
-        $db = getDBConnection();
-//        echo "no error2";
-    }
-    catch(Exception $exception){
-        echo "here error";
-        echo $exception->getMessage();
-    }
-
-//    echo "before foreeach";
-    foreach ($db->query('SELECT * FROM public.product') as $row)
-    {
-        echo "in foreach";
-      echo 'SKU: ' . $row['sku'];
-      echo 'ProductName: ' . $row['product_name'];
-      echo 'Description: ' . $row['description'];
-      echo '<br/>';
-    }
-  
-    // $db->query("select * from db.voterdb.sql")
- 
-    ?>
-    <form class="pure-form pure-form-aligned">
+<form class="pure-form pure-form-aligned">
         <fieldset>
             <div class="pure-control-group">
                 <label for="name">Username</label>
@@ -67,13 +38,3 @@
             </div>
         </fieldset>
     </form>
-
-    <!-- <form method="GET" action="scripture.php">
-        <input type="text" name="book">
-
-        <button type="submit">Submit</button>
-    </form> -->
-
-    </body>
-
-</html>
