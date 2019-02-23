9ead9a3fe3c65a4963b1ca1c69d9852b937c9a9e
Mathyouw <wil15171@byui.edu>
2019-02-22 19:03:09 -0700

    testing with teammates login files

--- web/login-register.php
+++ web/login-register.php
@@ -48,7 +48,7 @@
             </div>
         </fieldset>
     </form>
-<form action="PHPDataAccess.php" method="post">
+<form action="PHPDataAccess.php" method="get">
     Name: <input type="text" name="name"><br>
     E-mail: <input type="text" name="email"><br>
     <input type="submit">
