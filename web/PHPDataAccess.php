d1be46aa1f85f8642992a41652c74b612d1e82b6
Mathyouw <wil15171@byui.edu>
2019-02-24 14:52:25 -0700

    test2

--- web/PHPDataAccess.php
+++ web/PHPDataAccess.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+<!--    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="PHPDataAccessstylesheet.css">
     <meta charset="UTF-8">
     <title>Assignment05</title>
    
@@ -9,7 +10,7 @@
     <body>
 
     Welcome <?php echo $_POST["name"]; ?><br>
-    Your street address is: <?php echo $_POST["address"]; ?>
+<!--    Your street address is: --><?php //echo $_POST["address"]; ?>
 
     <?php
 
