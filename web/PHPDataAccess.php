4c8ddeba28c568b94a8c66fdd66ef54ac7ced335
Mathyouw <wil15171@byui.edu>
2019-03-05 21:54:14 -0700

    3/5/2019

--- web/PHPDataAccess.php
+++ web/PHPDataAccess.php
@@ -12,6 +12,18 @@
     <h1> Welcome <?php echo $_POST["name"]; ?><br></h1>
 
 <!--    Your street address is: --><?php //echo $_POST["address"]; ?>
+<!--    Array of images or numbers to pull from the db-->
+    <?php
+    $bg = array('bg-01.jpg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg' ); // array of filenames
+
+    $i = rand(0, count($bg)-1); // generate random number size of the array
+    $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+    ?>
+<!--    use this in the body -->
+
+    <!--background: url(images/--><?php //echo $selectedBg; ?><!--) no-repeat;-->
+
+
 
     <?php
 
