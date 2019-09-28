f71fae60bd123dc2ace0df743633bbc8a500b891
matt wilson <wil15171@byui.edu>
2019-09-28 09:50:24 -0600

    testing php

---
+++ web/w2PHP.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="PHPDataAccessstylesheet.css">
+    <meta charset="UTF-8">
+    <title>Assignment05</title>
+   
+</head>
+    <body class = content>
+<div class = paper>
+  
+
+
+    <?php
+
+    echo '<p> Please vote for your favorite item. </p>';?>
+<p>Generating a random number between 1 and 10:
+            <?php
+            echo rand(1, 10);
+            ?>
+        </p>
+
+</div>
+
+
+
+    </body>
+
+</html>
