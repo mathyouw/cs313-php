84392e40f9401d3ecf9e84b8aec221838b52419c
matt wilson <wil15171@byui.edu>
2019-10-24 21:33:40 -0600

    testing

--- web/week05/allergieshomepagetesting.php
+++ web/week05/allergieshomepagetesting.php
@@ -34,7 +34,7 @@ while (($row = $statement->fetch(PDO::FETCH_ASSOC)))
     $chapter = $row['ing_id'];
 
         echo "<tr>";
-        echo "<td> $book </td> <td> $row->ing_name </td>";
+        echo "<td> $book </td> <td>  $chapter </td>";
         echo"</tr>";
     // echo "<p><strong>$book $chapter $book[1] </strong></p>";
     // it worked until this line of code was added.
