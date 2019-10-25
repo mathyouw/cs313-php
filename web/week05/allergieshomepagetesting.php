18551aa3bb2751b6f4d17040fdeed03314be0735
matt wilson <wil15171@byui.edu>
2019-10-25 00:45:06 -0600

    testing

--- web/week05/allergieshomepagetesting.php
+++ web/week05/allergieshomepagetesting.php
@@ -37,7 +37,7 @@ while (($row = $statement->fetch(PDO::FETCH_ASSOC)))
 
         
         echo "<td> $book </td> <td>  $chapter </td> ";
-        echo "<td><input type='checkbox' name='checkbox_id' value='" . $book . "'> </td>";
+        echo "<td><input type='checkbox' name='checkbox_id[]' value='" . $book . "'> </td>";
     echo "</tr>";
        
     // echo "<p><strong>$book $chapter $book[1] </strong></p>";
