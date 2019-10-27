092803d6db198c0ae33f67a760f9d7eafe58ad0f
matt wilson <wil15171@byui.edu>
2019-10-26 23:45:24 -0600

    testing

--- web/week05/AddIngred.php
+++ web/week05/AddIngred.php
@@ -19,60 +19,18 @@ $db = get_db();
     <input type="text" name="email" placeholder="Email">
     <br>
     <input type="submit" value="Submit">
-</form>
+  </form>
   <div class="a">
   <h2>Add Ingradients</h2>
 
   <?php
   $ingredient1 = $_POST['ingredient1'];
   $statement = $db->prepare("INSERT INTO ingredients (ing_id, ing_name)
-  VALUES (100, " + "$ingredient1" +")");
+  VALUES (100, tofu)");
   $statement->execute();
 
   echo << "$ingredient1"; 
   ?> 
 
-
-
-
-
-
-
-
-
-<?php
-session_start();
-?>
- <form method="post" action="Checkout.php">
-   <?php
-   $checkbox_id = $_POST['checkbox_id'];
-   $camaro = $_POST['camaro'];
-   $F150 = $_POST['F150'];
-   $Lesabre = $_POST['Lesabre'];
-   $RV = $_POST['RV'];
-   $Fusion = $_POST['Fusion'];
-   $Honda = $_POST['Honda'];
-   foreach($_POST['checkbox_id'] as $check){
-    echo "$check<br>";
-   }
-   echo "$camaro<br>";
-   echo "$F150<br>";
-   echo "$Lesabre<br>";
-   echo "$RV<br>";
-   echo "$Fusion<br>";
-   echo "$Honda<br>";
-   $_SESSION['camaro'] = $camaro;
-   $_SESSION['F150'] = $F150;
-   $_SESSION['Lesabre'] = $Lesabre;
-   $_SESSION['RV'] = $RV;
-   $_SESSION['Fusion'] = $Fusion;
-   $_SESSION['Honda'] = $Honda;
-?>
- <input type="submit" value="Proceed to Checkout">
-</form>
-<form method="post" action="assignment03.php">
-<button onclick="goBack()">Return to Items List</button>
-</form>
-</div>
 </body>
 </html>
