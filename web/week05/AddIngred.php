48100cb63fd16b2be91f7159aa4f73eafcb1228f
matt wilson <wil15171@byui.edu>
2019-11-02 00:40:45 -0600

    trying to get Ingredworking

--- web/week05/AddIngred.php
+++ web/week05/AddIngred.php
@@ -14,7 +14,7 @@ $db = get_db();
 </head>
 
 <body background="food_pics\taco-chefcartoon.jpg">
-  <div style="background-color:lightblue; width: 500px;
+  <div align="center"; style="background-color:lightblue; width: 500px;
   margin: auto; border: double;">
    <h2>Please submit a recipe.</h2>
   <form method="post" action="AddIngred.php">
