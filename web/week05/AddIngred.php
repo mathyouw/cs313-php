6271d852bcdf064deea0ccfc4754888e2039ea80
matt wilson <wil15171@byui.edu>
2019-11-01 19:59:46 -0600

    trying to get Ingredworking

--- web/week05/AddIngred.php
+++ web/week05/AddIngred.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    require "dbConnect.php";
+    $db = get_db();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,21 +15,40 @@
 <body>
    <h2>Please submit a recipe.</h2>
   <form method="post" action="AddIngred.php">
-    <input type="text" name="Ingredient1" placeholder="Recipe Title">
+    <input type="text" name="recipetitle" placeholder="Recipe Title" required>
     <br>
-    <input type="text" name="email" placeholder="Recipe Ingredients">
+    <input type="text" name="recipe" placeholder="Recipe Ingredients" required>
     <br>
     <input type="submit" value="Submit">
   </form>
   <div class="a">
  
-
-
-
-
-
-
  
 </div>
+<?php
+
+ echo "Before isset <br>";
+    if(isset($_POST['submit'])){
+      echo "line 46 isset <br>";
+      $db = get_db();
+      $recipetitle = htmlspecialchars($_POST['recipetitle']);
+        $recipe = htmlspecialchars($_POST['recipe']);
+      try{
+        echo "line 56 isset <br>";
+        $result = $db->prepare("INSERT INTO users (PersonID, Username, Password, FirstName, LastName) 
+                             VALUES( :newid, :user, :pass, :fname, :lname)");
+        $result->bindParam('newid', $recipetitle);
+        $result->bindParam('fname', $recipe);
+        echo "line 76 try <br>";
+        echo $recipetitle."<br>";
+        echo $recipe."<br>";
+        $result->execute();
+        $rows = $result->fetch(PDO::FETCH_ASSOC);
+      }
+      catch (Exception $e) {
+        echo "Could not create new user". $e->getMessage();
+        exit();
+      }
+    }
 </body>
 </html>
