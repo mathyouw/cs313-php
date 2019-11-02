e6035953cade1d801af3af6a6025788e0a303eac
matt wilson <wil15171@byui.edu>
2019-11-02 15:39:14 -0600

    prettifying select allergies

--- web/week05/allergieshomepagetesting.php
+++ web/week05/allergieshomepagetesting.php
@@ -11,7 +11,7 @@ $db = get_db();
     <title>Assignment05</title>
     <link rel="stylesheet" type="text/css" href="03stylesheet.css">
 </head>
-<body background="food_pics\kelly-sikkema-VX0bsbyBxpM-unsplash.jpg" background-size: cover;>
+<body background="food_pics\kelly-sikkema-VX0bsbyBxpM-unsplash.jpg"; background-size: auto;>
     <div class="a"><br><br><br><H2>Allergy Manager</H2><br></div>
     <div align="center"; style="background-color:#0CAF9C; width: 500px;
   margin: auto; border: double;">
