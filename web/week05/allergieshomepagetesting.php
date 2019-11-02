61c8be33e7a25be427a7bf00bca25f8960499de0
matt wilson <wil15171@byui.edu>
2019-11-02 14:22:11 -0600

    prettifying select allergies

--- web/week05/allergieshomepagetesting.php
+++ web/week05/allergieshomepagetesting.php
@@ -36,11 +36,11 @@ while (($row = $statement->fetch(PDO::FETCH_ASSOC)))
     $book = $row['ing_name'];
     $chapter = $row['ing_id'];
     
-    echo $row;
+     if ($chapter % 3 == 0) {echo '<tr>'; }
         
         echo "<td> $book </td> <td>  $chapter </td> ";
         echo "<td><input type='checkbox' name='checkbox_id[]' value='" . $book . "'> </td>";
-    echo "</tr>";
+    if ($chapter % 3 == 0) {echo '</tr>';}
        
     // echo "<p><strong>$book $chapter $book[1] </strong></p>";
     // it worked until this line of code was added.
