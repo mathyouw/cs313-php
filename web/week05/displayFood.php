2511a36d69f6a09d4859cee028f40c0bbcb3bedf
matt wilson <wil15171@byui.edu>
2019-11-02 18:33:05 -0600

    adding stuff

--- web/week05/displayFood.php
+++ web/week05/displayFood.php
@@ -52,15 +52,7 @@ session_start();
     }
 
    }
-   if ($visiblepancake == true;){
-    // echo '<div> Pancake </div>';
-   }
-   if ($visiblelasagna == true;){
-    // echo '<div> Lasagna </div>';
-   }
-   if ($visiblesoup == true;){
-    // echo '<div> SOUP </div>';
-   }
+  
     
   
 ?>
