cc8cefaf8cb9992e15733b408c8b988c933a5354
matt wilson <wil15171@byui.edu>
2019-11-02 18:11:35 -0600

    prettifying select allergies

--- web/week05/displayFood.php
+++ web/week05/displayFood.php
@@ -8,6 +8,20 @@
       
 </head>
 <body>
+<?php>
+$pancake = array("milk", "vinegar", "flour", "sugar", "baking powder", "baking soda", "salt", "egg", "butter");
+$lasagna = array("sausage", "ground beef", "onion","garlic","tomatoes
+","white sugar","basil leaves","fennel seeds","Italian seasoning salt
+","black pepper","parsley ","lasagna noodles","ricotta cheese","egg
+","mozzarella cheese","Parmesan cheese");
+$soup = array("Potatoes","celery","onion","Ham","white pepper","black pepper","butter
+","milk");
+
+
+
+
+?>
+
    <div class="a">
    <h2>Foods you can't eat.</h2>
    <h1>We're Sorry This portion is still in development.</h1>
@@ -19,6 +33,9 @@ session_start();
    $checkbox_id = $_POST['checkbox_id'];
    foreach($_POST['checkbox_id'] as $check){
     echo "$check<br>";
+    if (in_array($check, $pancake)) {
+    echo "Got No pancake";
+    }
    }
   
 ?>
